fa5138af1cf16b609ba205926e8071e1a4a6d233
anton banshchikov <banshant@yandex.ru>
2023-02-16 13:30:27 +0300

    change cli.js

--- src/index.js
+++ src/index.js
@@ -1,7 +1,7 @@
 import readlineSync from 'readline-sync';
-//Ввод кол-ва раундов
+// Ввод кол-ва раундов
 const roundsCount = 3;
-//Приветсвие игрока
+// Приветсвие игрока
 const run = (getRound, description) => {
   console.log('Welcome to the Brain Games!');
   const name = readlineSync.question('May I have your name? ');
@@ -11,7 +11,7 @@ const run = (getRound, description) => {
     const [question, correctAnswer] = getRound();
     console.log(`Question: ${question}`);
     const userAnswer = readlineSync.question('Your answer: ');
-//Проверка на правильность ответа
+    // Проверка на правильность ответа
     if (userAnswer !== correctAnswer) {
       console.log(`${userAnswer} is wrong answer ;(. Correct answer was ${correctAnswer}.`);
       console.log(`Let's try again, ${name}!`);
